a8924c9202854f055e7fcebb82a88151440ebc2a
yangaobiao <312710721@qq.com>
2020-11-10 16:28:51 +0800

    bug fix:delete

--- src/main/java/com/webank/weevent/demo/JavaSDKSample.java
+++ src/main/java/com/webank/weevent/demo/JavaSDKSample.java
@@ -8,9 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Before;
-import org.springframework.context.annotation.Bean;
-
 import com.webank.weevent.client.BrokerException;
 import com.webank.weevent.client.IWeEventClient;
 import com.webank.weevent.client.JsonHelper;
